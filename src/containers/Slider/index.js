76f422fb94181e92d27d8d220c403ea95492761d
FenrirRM <rehoudja.mael@gmail.com>
2024-01-05 02:45:42 +0100

    correction Slider et Date

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,13 +8,22 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+
+    // Changement de l'opérateur logique pour trier dans le bon sens les images 
+    // De la plus ancienne à la plus récente
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
+
+    // Ajout condition pour gérer l'erreur console (byDateDesc.lenght : undefined)
+    if (byDateDesc) {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+
+      // Suppression de l'élément vide du carrousel en ajoutant -1 à la taille du tableau
+      () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
       5000
     );
+    }
   };
   useEffect(() => {
     nextCard();
@@ -22,14 +31,16 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+
+        // Déplacement de la key dans une autre div qui englobe le composant slidecard
+        <div key={event.title}>
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            {/* Ajout d'un alt unique pour chaque images */}
+            <img src={event.cover} alt={event.title} />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
@@ -40,17 +51,24 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+
+              {/* Modification des params et de la key (corrige erreur console pas de key unique) */}
+              {byDateDesc.map((bulletPoint, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${bulletPoint.title}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+
+                  // Remplacement de idx par index
+                  checked={index === radioIdx}
+                  
+                  // Ajout readOnly pour gérer erreur console : checked without onChange
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
